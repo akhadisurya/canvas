d90a7d9b8bed5654bbbaf0cebc63fe624e84aae3
Arvin Hadisurya <akhadisurya@gmail.com>
2016-06-16 18:16:36 -0700

    fifth

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
+var leather = new Image();
+leather.src="arvin.jpg"
+
 function draw(){
   var ctx = document.getElementById('my_canvas').getContext('2d');
-  ctx.fillStyle = "magenta";
-  ctx.fillRect(20, 20, 100, 100);
-  ctx.fillRect(200, 200, 50, 50);
-  ctx.strokeRect(20, 20, 100, 100);
-  ctx.strokeRect(200, 200, 50, 50);
-  // ctx.clearRect()
+  var g1 = ctx.createLinearGradient(0, 0, 200, 0);
+  g1.addColorStop(0, "magenta");
+  g1.addColorStop(1, "black");
+
+  ctx.fillStyle = g1;
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 10;
+  ctx.fillRect(0, 0, 200, 200);
+  ctx.strokeRect(0, 0, 200, 200);
+
+  var pattern = ctx.createPattern(leather, "repeat-x")
+  ctx.fillStyle = pattern;
+  ctx.fillRect(0, 0, 200, 200);
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = g1;
+  ctx.strokeRect(0, 0, 200, 200);
+  ctx.fill();
 }
 
 window.onload = draw;
