02c680b0281df48dbacf0a9910d8cf66efe7d327
Arvin Hadisurya <akhadisurya@gmail.com>
2016-06-16 17:51:05 -0700

    fourth

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 function draw(){
-  var ctx = document.getElementById('my_canvas').getContext('2d')
+  var ctx = document.getElementById('my_canvas').getContext('2d');
+  ctx.fillStyle = "magenta";
+  ctx.fillRect(20, 20, 100, 100);
+  ctx.fillRect(200, 200, 50, 50);
+  ctx.strokeRect(20, 20, 100, 100);
+  ctx.strokeRect(200, 200, 50, 50);
+  // ctx.clearRect()
 }
 
 window.onload = draw;
